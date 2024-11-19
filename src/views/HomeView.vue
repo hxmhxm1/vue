d818ed2a04348414359f93c00f22c62fb3c0809d
huangxiaomin <huangxiaomin@sensetime.com>
2024-11-19 14:49:39 +0800

    feat: 修改了主页样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,84 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>首页</main>
+  <div class="content">
+    <div v-for="(item, index) in data" :key="index" 
+      @click="goDetail(item)" class="card" 
+      :style="{background: randomColor()}"
+    >
+      <div class="title">{{item.title}}</div>
+      <div class="grey">{{item.desc}}</div>
+    </div>
+  </div>
+  <RouterView/>
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { reactive } from 'vue';
+const router = useRouter()
+const data = reactive([
+  {
+    title: 'provide、inject', 
+    url: '/about',
+    desc: '等同于react中的context'
+  },
+  {
+    title: '待定', 
+    url: '/',
+    desc: ''
+  }
+])
+const goDetail = (params: {title: string, url: string, desc: string}) => {
+  router.push(params.url)
+}
+// 生成随机颜色 
+const randomColor = () => {
+  function isLightColor(color) {
+    // 判断颜色是否为浅色
+    const rgb = parseInt(color.substring(1), 16);
+    const r = (rgb >> 16) & 0xff;
+    const g = (rgb >> 8) & 0xff;
+    const b = (rgb >> 0) & 0xff;
+    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return luma > 170; // 调整阈值以适应你定义的浅色范围
+  }
+  // 生成浅色系随机颜色
+  let color = '#';
+  while(!isLightColor(color)){
+    color = '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
+  return color;
+}
+</script>
+
+<style scoped>
+.content{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title{
+  font-size: 20px;
+  font-weight: 700;
+}
+.desc{
+  font-size: 16px;
+  color: grey;
+}
+.card{
+  width: 200px;
+  height: 200px;
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: 2px double #333; /* 实线边框，颜色为 #333 */
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1); /* 阴影 */
+  transition: border-color 0.3s ease; /* 边框颜色变化动画 */
+  border-radius: 5px;
+}
+.card:hover {
+  border-color: yellow; /* 鼠标悬停时的边框颜色 */
+}
+</style>
